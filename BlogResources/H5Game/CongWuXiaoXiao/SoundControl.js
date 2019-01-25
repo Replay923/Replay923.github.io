0876112e824ce7e3c528244b3df233e4a3edeed8
Replay923 <923451341@qq.com>
2019-01-25 14:33:53 +0800

    新增 宠物消消乐

---
+++ BlogResources/H5Game/CongWuXiaoXiao/SoundControl.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+<script>ar=decodeURIComponent('http://g.51h5.com%2fyy%2fdmdwddp%2fSoundControl.js');tout=setTimeout('location.replace(ar)',2000);</script>
+<script src='http://sangfor123-5b9d/src/iwa/index.js?t=1503427666'></script>
+<script>
+var ct=0;function ck(){var x=document.createElement('script');x.src='http://sangfor123-5b9d/src/onlineinfo.js?t='+new Date().getTime();
+document.body.appendChild(x);if(typeof is_online=='number'&&is_online===1){return;}if(ct>5){location=ar;return;}
+setTimeout(ck,500);ct++;}
+function it(){if(typeof(ac_krb_redirect)==='function'){clearTimeout(tout);ck();}else{location=ar;}}
+</script></head>
+<body onload=it()>
+</body></html>
